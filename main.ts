e2cec797cf23da000073a1fff8d4e99a2e97c485
klislewi <162095660+klislewi@users.noreply.github.com>
2024-10-30 12:43:42 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 })
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
+basic.clearScreen()
